078761a44d0a2fc9816262b2c1edac9b58b913ad
Robert Koronczi <robert.koronczi.development@gmail.com>
2019-07-24 19:44:02 +0200

    app.js problem solved

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(folder));
 
-app.get('/de', (req, res) => {
-    res.send(path.join(__dirname, 'i.html'));
-})
-
 app.listen(port, () => {
     console.log(`Server is running on the port ${port}`);
 })
